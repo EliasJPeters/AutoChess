a1d747f985390291e7e215a2a4e6b0eabaa7e744
EliasJPeters <eligreenmen@gmail.com>
2022-11-22 21:15:01 -0500

    Update

--- speech.py
+++ speech.py
@@ -5,13 +5,19 @@ from chess import *
 # obtain audio from the microphone
 r = sr.Recognizer()
 with sr.Microphone(device_index=1) as source:
+    r.adjust_for_ambient_noise(source) #only do this once before we start listening
     print("Say something!")
     audio = r.listen(source)
+
+
+
+
 
 
 WIT_AI_KEY = "3SJGCEANYYUJQSVMTMCNGVC4XX22LMMW"  # Wit.ai keys are 32-character uppercase alphanumeric strings
 try:
     print("Did you say: " + r.recognize_wit(audio, key=WIT_AI_KEY))
+    
 except sr.UnknownValueError:
     print("Could not understand audio")
 except sr.RequestError as e:
