d7ee0ed8a215d148641a4ae4236c24ac4aeafeab
EliasJPeters <eligreenmen@gmail.com>
2022-11-22 21:02:59 -0500

    Upload the initial files

---
+++ chess.py
@@ -0,0 +1,10 @@
+from stockfish import Stockfish
+
+from speech import *
+
+stockfish = Stockfish(path="C:/Users\Elias' Gaming PC\Downloads\stockfish_15_win_x64_avx2\stockfish_15_x64_avx2.exe")
+
+
+
+
+
---
+++ speech.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+
+from chess import *
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone(device_index=1) as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+
+
+
+
+
+WIT_AI_KEY = "3SJGCEANYYUJQSVMTMCNGVC4XX22LMMW"  # Wit.ai keys are 32-character uppercase alphanumeric strings
+try:
+    print("Did you say: " + r.recognize_wit(audio, key=WIT_AI_KEY))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Wit.ai service; {0}".format(e))
