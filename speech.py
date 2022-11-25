6b954779c3e6d938572ea5a3b59dd0bf1b379696
EliasJPeters <eligreenmen@gmail.com>
2022-11-24 21:59:21 -0500

    Added stockfish engine to code files to support running on computers other than one originally programmed.

--- speech.py
+++ speech.py
@@ -4,16 +4,18 @@ from chess import *
 
 # obtain audio from the microphone
 r = sr.Recognizer()
-with sr.Microphone(device_index=1) as source:
-    r.adjust_for_ambient_noise(source) #only do this once before we start listening
-    print("Say something!")
-    audio = r.listen(source)
-
 WIT_AI_KEY = "3SJGCEANYYUJQSVMTMCNGVC4XX22LMMW"  # Wit.ai keys are 32-character uppercase alphanumeric strings
-try:
-    print("Did you say: " + r.recognize_wit(audio, key=WIT_AI_KEY))
-    
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Wit.ai service; {0}".format(e))
+
+
+def listenAudio():
+    with sr.Microphone(device_index=1) as source:
+        r.adjust_for_ambient_noise(source) #only do this once before we start listening
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        print("Did you say: " + r.recognize_wit(audio, key=WIT_AI_KEY))
+        translatedAudio = r.recognize_wit(audio, key = WIT_AI_KEY)
+    except sr.UnknownValueError:
+        print("Could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Wit.ai service; {0}".format(e))
