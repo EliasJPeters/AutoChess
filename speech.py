1b8443458263703f03c85ea17332802c09cbb4de
EliasJPeters <108840264+EliasJPeters@users.noreply.github.com>
2024-02-14 22:55:51 -0500

    Update speech.py

--- speech.py
+++ speech.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 def listenAudio():
     # obtain audio from the microphone
     r = sr.Recognizer()
-    WIT_AI_KEY = "3SJGCEANYYUJQSVMTMCNGVC4XX22LMMW"  # Wit.ai keys are 32-character uppercase alphanumeric strings
+    WIT_AI_KEY = ""  # Wit.ai keys are 32-character uppercase alphanumeric strings
     with sr.Microphone(device_index=4) as source:
         r.adjust_for_ambient_noise(source) #only do this once before we start listening
         print("Listening...")
